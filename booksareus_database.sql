0351b8dc3699458d80ccb51a3af1d3e60ea36eb7
samturton2 <STurton@spartaglobal.com>
2020-10-27 12:03:58 +0000

    data inserted unto database

--- booksareus_database.sql
+++ booksareus_database.sql
@@ -3,7 +3,7 @@ USE booksareus;
 
 CREATE TABLE users 
 (
-    user_id INT NOT NULL,
+    user_id INT NOT NULL IDENTITY(1,1),
     first_name VARCHAR(64) NOT NULL,
     last_name VARCHAR(64) NOT NULL,
     phone_number VARCHAR(11) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE users
 
 CREATE TABLE ebooks
 (
-    ebook_id INT NOT NULL,
+    ebook_id INT NOT NULL IDENTITY(1,1),
     title VARCHAR(64) NOT NULL,
     ebook_location VARCHAR(64) NOT NULL,
     release_date DATE NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE ebooks
 
 CREATE TABLE bookings
 (
-    booking_id INT NOT NULL,
+    booking_id INT NOT NULL IDENTITY(1,1),
     ebook_id INT NOT NULL,
     user_id INT NOT NULL,
     booking_date DATE NOT NULL,
@@ -33,3 +33,27 @@ CREATE TABLE bookings
     FOREIGN KEY (ebook_id) REFERENCES ebooks(ebook_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+-- Input data into user
+INSERT INTO users(first_name, last_name, phone_number, email);
+VALUES ('Jane', 'McLewsky', '07695456407', 'J.McLew@yahoo.com');
+
+INSERT INTO users(first_name, last_name, phone_number, email);
+VALUES ('Micheal', 'Leach', '07695488407', 'mikeleach@gmail.com');
+
+-- Input data into ebooks
+INSERT INTO ebooks(title, ebook_location, release_date, user_id);
+VALUES ('Naughts and crosses', 'N123', '08/24/98', 1)
+
+INSERT INTO ebooks(title, ebook_location, release_date, user_id);
+VALUES ('Thunderstruck', 'T89', '12/04/98', 2)
+
+-- Input data into bookings
+INSERT INTO bookings(ebook_id, user_id, booking_date, price);
+VALUES (1, 2, '11/04/16', '4.99')
+
+INSERT INTO bookings(ebook_id, user_id, booking_date, price);
+VALUES (2, 1, '01/30/18', '8.99')
+
+INSERT INTO bookings(ebook_id, user_id, booking_date, price);
+VALUES (2, 2, '20/30/18', '8.99')
